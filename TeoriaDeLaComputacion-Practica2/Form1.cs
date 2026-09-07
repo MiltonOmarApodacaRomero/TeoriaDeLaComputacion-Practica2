268e2302fec284ca30ea732300f9482998ee5105
MiltonColdy <miltonapodacar15@gmail.com>
2026-09-06 23:18:46 -0700

    Cambios finales

--- TeoriaDeLaComputacion-Practica2/Form1.cs
+++ TeoriaDeLaComputacion-Practica2/Form1.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
 namespace TeoriaDeLaComputacion_Practica2;
+
 
 public partial class Form1 : Form
 {
-   
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowDisplayAffinity(IntPtr hWnd, uint dwAffinity);
+    
     public Form1()
     {
         InitializeComponent();
@@ -10,6 +17,7 @@ public partial class Form1 : Form
 
     private void Form1_Load(object sender, EventArgs e)
     {
+        SetWindowDisplayAffinity(this.Handle, 0x00000000);
         Lexor.Setup(this);
     }
 
